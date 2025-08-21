c84117d024f5698679b541c45cb35765099e0bc2
Yuma <yuma248@gmail.com>
2025-08-21 10:50:27 +0930

    Update circle-plot.js

--- js/circle-plot.js
+++ js/circle-plot.js
@@ -456,12 +456,12 @@ Assembly.prototype.drawPlot = function(parent_div, longest, circle_span) {
       .attr("id", "asm-g-busco_completeness_legend");
     var txt = lccg.append('text')
       //.attr('transform', 'translate(' + (size / 2 - 210) + ',' + (-size / 2 + 20) + ')')
-      .attr('transform', 'translate(' + (-size / 2 - 10) + ',' + (size / 2 + 70) + ')')
+      .attr('transform', 'translate(' + (-size / 2 + 10) + ',' + (size / 2 - 70) + ')')
       .attr('class', 'asm-br_title');
     //  .attr('class', 'asm-tr_title');
     txt.append('tspan').text('BUSCO (n = ' + this.busco.n.toLocaleString() + ')');
     //var key = lccg.append('g').attr('transform', 'translate(' + (size / 2 - 210) + ',' + (-size / 2 + 28) + ')');
-    var key = lccg.append('g').attr('transform', 'translate(' + (-size / 2 - 10) + ',' + (size / 2 + 62) + ')');
+    var key = lccg.append('g').attr('transform', 'translate(' + (-size / 2 + 10) + ',' + (size / 2 - 62) + ')');
     key.append('rect').attr('height', w).attr('width', w).attr('class', 'asm-busco_C asm-toggle');
     key.append('text').attr('x', w + 3).attr('y', w - 1).text('Comp. (' + this.busco.C.toFixed(1) + '%)').attr('class', 'asm-key');
     key.append('rect').attr('y', w * 1.5).attr('height', w).attr('width', w).attr('class', 'asm-busco_D asm-toggle');
